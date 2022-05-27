72cf16d25d589a9bf17d88a034b59e999683d1c0
Justin Sherrill <jsherril@redhat.com>
2022-05-27 15:02:42 -0400

    CONTENT-59: integrate viper for config loading

--- README.md
+++ README.md
@@ -6,24 +6,57 @@ Content Sources is an application for storing information about external content
 
 ##Developing
 
-Create a `.env` file with:
-~~~
-DATABASE_HOST=localhost
-DATABASE_PORT=5432
-DATABASE_USER=content
-DATABASE_PASSWORD=content
-DATABASE_NAME=content
-~~~
+### Configuring Postgresql
 
-pointed to your postgresql db.
+As root run: 
+```shell
+yum install -y postgresql
 
+echo "host    all             all             127.0.0.1/32            trust" >> /var/lib/pgsql/data/pg_hba.conf
+echo "host    all             all             ::1/128                 trust" >> /var/lib/pgsql/data/pg_hba.conf
+systemctl start postgresql
+systemctl enable postgresql
+sudo -u postgres createdb content
+sudo -u postgres psql -c "CREATE USER content WITH PASSWORD 'content'"
+sudo -u postgres psql -c "grant all privileges on database content TO content"
+```
 
+### Create your configuration
+Create a config file from the example:
+```
+cp ./configs/config.yaml.example ./configs/config.yaml
+```
+
+### Migrate your database (and seed it if desired)
+```
+go run ./cmd/dbmigrate/main.go up
+```
+```
+go run ./cmd/dbmigrate/main.go seed
+```
+
+### Run the server!
+
+```
+go run ./cmd/content-sources/main.go
+```
+
+###
+Hit the api:
+
+```
+curl http://localhost:8000/api/content_sources/v1.0/repositories/ ```
+```
 ### Generating new openapi docs:
 
 ~~~
 go install github.com/swaggo/swag/cmd/swag@latest
 make openapi
 ~~~
+
+### Configuration
+
+The default configuration file in ./configs/config.yaml.example shows all available config options.  Any of these can be overridden with an environment variable.  For example  "database.name" can be passed in via an environment variable named "DATABASE_NAME".
 
 ### Contributing
  * Pull requests welcome!
--- pkg/config/config.go
+++ pkg/config/config.go
@@ -2,43 +2,116 @@ package config
 
 import (
 	"os"
+	"strings"
 
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 	clowder "github.com/redhatinsights/app-common-go/pkg/api/v1"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
+	"github.com/ziflex/lecho/v3"
 )
 
-type Config struct {
-	DBHost     string
-	DBPort     int
-	DBUser     string
-	DBPassword string
-	DBName     string
+type Configuration struct {
+	Database Database
+	Logging  Logging
+	Loaded   bool
 }
 
-func Get() *Config {
-	options := viper.New()
+type Database struct {
+	Host     string
+	Port     int
+	User     string
+	Password string
+	Name     string
+}
+
+type Logging struct {
+	Level   string
+	Console bool
+}
+
+var LoadedConfig Configuration
+
+func Get() *Configuration {
+	if !LoadedConfig.Loaded {
+		Load()
+	}
+	return &LoadedConfig
+}
+
+func readConfigFile(v *viper.Viper) {
+	v.SetConfigName("config")
+	v.SetConfigType("yaml")
+	v.AddConfigPath("./configs/")
+
+	path, set := os.LookupEnv("CONFIG_PATH")
+	if set {
+		v.AddConfigPath(path)
+	}
+	err := v.ReadInConfig()
+	if err != nil {
+		log.Logger.Err(err)
+	}
+}
+
+func setDefaults(v *viper.Viper) {
+	v.SetDefault("Loaded", true)
+	//In viper you have to set defaults, otherwise loading from ENV doesn't work
+	//   without a config file present
+	v.SetDefault("database.host", "")
+	v.SetDefault("database.port", "")
+	v.SetDefault("database.user", "")
+	v.SetDefault("database.password", "")
+	v.SetDefault("database.name", "")
+}
+
+func Load() {
+	v := viper.New()
+
+	readConfigFile(v)
+	v.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
+	v.AutomaticEnv()
+	setDefaults(v)
 
 	if clowder.IsClowderEnabled() {
 		cfg := clowder.LoadedConfig
-
-		options.SetDefault("DBHost", cfg.Database.Hostname)
-		options.SetDefault("DBPort", cfg.Database.Port)
-		options.SetDefault("DBUser", cfg.Database.Username)
-		options.SetDefault("DBPassword", cfg.Database.Password)
-		options.SetDefault("DBName", cfg.Database.Name)
-	} else {
-		options.SetDefault("DBHost", os.Getenv("DATABASE_HOST"))
-		options.SetDefault("DBPort", os.Getenv("DATABASE_PORT"))
-		options.SetDefault("DBUser", os.Getenv("DATABASE_USER"))
-		options.SetDefault("DBPassword", os.Getenv("DATABASE_PASSWORD"))
-		options.SetDefault("DBName", os.Getenv("DATABASE_NAME"))
+		v.Set("database.host", cfg.Database.Hostname)
+		v.Set("database.port", cfg.Database.Port)
+		v.Set("database.user", cfg.Database.Username)
+		v.Set("database.password", cfg.Database.Password)
+		v.Set("database.name", cfg.Database.Name)
 	}
 
-	return &Config{
-		DBHost:     options.GetString("DBHost"),
-		DBPort:     options.GetInt("DBPort"),
-		DBUser:     options.GetString("DBUser"),
-		DBPassword: options.GetString("DBPassword"),
-		DBName:     options.GetString("DBName"),
+	err := v.Unmarshal(&LoadedConfig)
+	if err != nil {
+		panic(err)
 	}
+}
+
+func ConfigureLogging() {
+	level, err := zerolog.ParseLevel(Get().Logging.Level)
+	if err != nil {
+		log.Error().Err(err)
+	}
+
+	if Get().Logging.Console {
+		log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	}
+	log.Logger.Level(level)
+}
+
+func ConfigureEcho() *echo.Echo {
+	e := echo.New()
+	echoLogger := lecho.From(log.Logger,
+		lecho.WithTimestamp(),
+		lecho.WithCaller(),
+	)
+
+	e.Use(middleware.RequestID())
+	e.Use(lecho.Middleware(lecho.Config{
+		Logger: echoLogger,
+	}))
+	return e
 }
