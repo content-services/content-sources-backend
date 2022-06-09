82c1bb6ef23a7b356524fa3b85255dfa957227b5
Alejandro Visiedo <avisiedo@redhat.com>
2022-06-09 06:30:32 +0200

    makefile:update rule to install swag locally more isolated
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- mk/swag.mk
+++ mk/swag.mk
@@ -1,20 +1,28 @@
 ##
 # Rules to build openapi specification from the source code
+# and to install swag locally.
 ##
 
 SWAG=$(GO_OUTPUT)/swag
 
+.PHONY: install-swag  ## Install swag locally on your GO_OUTPUT (./release) directory
+install-swag: $(SWAG)
+
+$(SWAG): GOPATH:=$(shell mktemp -d "$(PROJECT_DIR)/tmp.XXXXXXXX" 2>/dev/null)
 $(SWAG):
-	{\
-		export GOPATH=$(shell mktemp -d "$$PWD/tmp.XXXXXXXX") ; \
-		export GOBIN=$(dir $(SWAG)) ; \
+	@{\
+		echo "Using GOPATH='$(GOPATH)'" ; \
+		export GOPATH="$(GOPATH)" ; \
+		[ "$(GOPATH)" != "" ] || { echo "error:GOPATH is empty"; exit 1; } ; \
+		export GOBIN="$(dir $(SWAG))" ; \
+		echo "Installing 'swag' at '$(SWAG)'" ; \
 		go install github.com/swaggo/swag/cmd/swag@latest ; \
-		find "$${GOPATH}" -type d -exec chmod u+w {} \; ; \
-		rm -rf "$${GOPATH}" ; \
+		find "$(GOPATH)" -type d -exec chmod u+w {} \; ; \
+		rm -rf "$(GOPATH)" ; \
 	}
 
 .PHONY: openapi
-openapi: $(SWAG) ## Generate the openapi from the source code
+openapi: swag ## Generate the openapi from the source code
 	$(SWAG) init --generalInfo api.go --o ./api --dir pkg/handler/ --pd pkg/api
 	# Convert from swagger to openapi
 	go run ./cmd/swagger2openapi/main.go api/swagger.json api/openapi.json
