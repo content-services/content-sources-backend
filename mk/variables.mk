dc7040d5c847751ce979ba4b76378b2a32cbec45
Alejandro Visiedo <avisiedo@redhat.com>
2022-06-09 05:53:36 +0200

    makefile:read database config from configs/config.yaml
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- mk/variables.mk
+++ mk/variables.mk
@@ -12,14 +12,40 @@
 # variable value when invoking 'make' command.
 ##
 
-## Database variables (expected from mk/private.mk or .env)
+
+## Database variables (expected from configs/config.yaml)
 # Here just indicate that variables should be exported
+ifeq (,$(shell yq --version 2>/dev/null))
+$(info 'yq' can not be found in your environment)
+$(info to keep synced the config/config.yaml file with)
+$(info DATABASE_* variables)
+$(info You can install it by 'pip install yq')
+$(error Missed 'yq' tool)
+endif
+
+# Retrieve values from 'configs/config.yaml' file
+DATABASE_HOST:=$(shell yq -r -M '.database.host' "$(PROJECT_DIR)/configs/config.yaml")
+DATABASE_PORT:=$(shell yq -M '.database.port' "$(PROJECT_DIR)/configs/config.yaml")
+DATABASE_NAME:=$(shell yq -r -M '.database.name' "$(PROJECT_DIR)/configs/config.yaml")
+DATABASE_USER:=$(shell yq -r -M '.database.user' "$(PROJECT_DIR)/configs/config.yaml")
+DATABASE_PASSWORD:=$(shell yq -r -M '.database.password' "$(PROJECT_DIR)/configs/config.yaml")
+
+# Set some default values
+DATABASE_HOST ?= localhost
+DATABASE_PORT ?= 5432
+DATABASE_NAME ?= content
+DATABASE_USER ?= content
+DATABASE_PASSWORD ?= content
+
+# Make the values availables for the forked processes as env vars
 export DATABASE_HOST
 export DATABASE_PORT
 export DATABASE_NAME
 export DATABASE_USER
 export DATABASE_PASSWORD
 
+
+## Binary output
 # The directory where golang will output the
 # built binaries
 GO_OUTPUT ?= $(PROJECT_DIR)/release
@@ -39,4 +65,3 @@ DOCKER_IMAGE_BASE ?= quay.io/$(QUAY_USER)/content-sources
 DOCKER_IMAGE_TAG ?= $(shell git rev-parse --short HEAD)
 # Compose the container image with all the above
 DOCKER_IMAGE ?= $(DOCKER_IMAGE_BASE):$(DOCKER_IMAGE_TAG)
-
