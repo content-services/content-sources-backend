cdb9f09b5e1065aaa594e35e1b328795a782c49e
Alejandro Visiedo <avisiedo@redhat.com>
2022-06-15 15:13:42 +0200

    db:update database gorm models
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- pkg/models/repository_rpm.go
+++ pkg/models/repository_rpm.go
@@ -3,18 +3,18 @@ package models
 type RepositoryRpm struct {
 	Base
 	// The rpm package name
-	Name string `json:"name" gorm:"index:pkgname;not null"`
+	Name string `json:"name" gorm:"not null"`
 	// The architecture that this package belong to
-	Arch string `json:"arch" gorm:"primaryKey;not null"`
+	Arch string `json:"arch" gorm:"not null"`
 	// The version for this package
-	Version string `json:"version" gorm:"primaryKey;not null"`
+	Version string `json:"version" gorm:"not null"`
 	// The release for this package
-	Release string `json:"release" gorm:"primaryKey;null"`
+	Release string `json:"release" gorm:"null"`
 	// Epoch is a way to define weighted dependencies based
 	// on version numbers. It's default value is 0 and this
 	// is assumed if an Epoch directive is not listed in the RPM SPEC file.
 	// https://access.redhat.com/documentation/en-us/red_hat_enterprise_linux/8/html/packaging_and_distributing_software/advanced-topics#packaging-epoch_epoch-scriplets-and-triggers
-	Epoch *int32 `json:"epoch" gorm:"default:0;not null"`
-	// RepositoryConfig reference
-	Repo RepositoryConfiguration `json:"repository_config_id" gorm:"foreignkey:uuid"`
+	Epoch      *int32     `json:"epoch" gorm:"default:0;not null"`
+	Refer2Repo string     `gorm:"not null"`
+	Repo       Repository `gorm:"foreignKey:UUID;references:Refer2Repo"`
 }
