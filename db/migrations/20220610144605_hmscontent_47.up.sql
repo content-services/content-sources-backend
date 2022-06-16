a810d9b48e568c28e70d51672732b054ec15338a
Alejandro Visiedo <avisiedo@redhat.com>
2022-06-16 16:47:23 +0200

    db:fix upgrade sql script and update linter hints from sqlfluff
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- db/migrations/20220610144605_hmscontent_47.up.sql
+++ db/migrations/20220610144605_hmscontent_47.up.sql
@@ -1,4 +1,3 @@
-
 BEGIN;
 
 --
@@ -6,34 +5,40 @@ BEGIN;
 --
 CREATE TABLE IF NOT EXISTS repositories (
     uuid UUID UNIQUE NOT NULL PRIMARY KEY,
-    created_at timestamp with time zone,
-    updated_at timestamp with time zone,
+    created_at TIMESTAMP WITH TIME ZONE,
+    updated_at TIMESTAMP WITH TIME ZONE,
     url VARCHAR(255) NOT NULL,
-    last_read_time timestamp with time zone,
-    last_read_error timestamp with time zone,
-    refer2_repo_config UUID NOT NULL
+    last_read_time TIMESTAMP WITH TIME ZONE,
+    last_read_error TIMESTAMP WITH TIME ZONE,
+    refer_repo_config UUID NOT NULL
 );
+
+ALTER TABLE ONLY repositories
+ADD CONSTRAINT fk_repositories
+FOREIGN KEY (refer_repo_config)
+REFERENCES repository_configurations(uuid);
 
 --
 -- repository_rpms
 --
 CREATE TABLE IF NOT EXISTS repository_rpms (
     uuid UUID UNIQUE NOT NULL PRIMARY KEY,
-    created_at timestamp with time zone,
-    updated_at timestamp with time zone,
+    created_at TIMESTAMP WITH TIME ZONE,
+    updated_at TIMESTAMP WITH TIME ZONE,
     name VARCHAR(255) NOT NULL,
     arch VARCHAR(255) NOT NULL,
     version VARCHAR(255) NOT NULL,
     release VARCHAR(255),
-    epoch integer DEFAULT 0 NOT NULL,
-    repo_refer UUID NOT NULL
+    epoch INTEGER DEFAULT 0 NOT NULL,
+    refer_repo UUID NOT NULL
 );
 
 -- ALTER TABLE ONLY repository_rpms
 --     ADD CONSTRAINT repository_rpms_pkey PRIMARY KEY (uuid);
 
 ALTER TABLE ONLY repository_rpms
-    ADD CONSTRAINT fk_repositories
-    FOREIGN KEY (repo_refer) REFERENCES repositories(uuid);
+ADD CONSTRAINT fk_repositories_rpms
+FOREIGN KEY (refer_repo) REFERENCES repositories(uuid)
+ON DELETE CASCADE;
 
 COMMIT;
