7e2a63cc23f5e08c27fc5cd6d0d8bcb97bb3b512
Alejandro Visiedo <avisiedo@redhat.com>
2022-06-17 15:13:00 +0200

    api:update error handling for the handler
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- pkg/handler/repositories_rpm.go
+++ pkg/handler/repositories_rpm.go
@@ -1,7 +1,7 @@
 package handler
 
 import (
-	"encoding/json"
+	"net/http"
 
 	"github.com/content-services/content-sources-backend/pkg/api"
 	"github.com/content-services/content-sources-backend/pkg/dao"
@@ -28,40 +28,52 @@ func RegisterRepositoryRpmRoutes(engine *echo.Group, rDao *dao.RepositoryDao) {
 // @Summary      List Repositories RPMS
 // @ID           listRepositoriesRpms
 // @Description  get repositories RPMS
-// @Tags         repositories
+// @Tags         repositories,rpms
 // @Accept       json
 // @Produce      json
 // @Success      200 {object} api.RepositoryRpmCollectionResponse
 // @Router       /repositories/:uuid/rpms [get]
 //
 func listRepositoriesRpm(c echo.Context) error {
+	// Read input information
 	var rpmInput RPMRequest
 	if err := (&echo.DefaultBinder{}).BindPathParams(c, &rpmInput); err != nil {
-		return err
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	}
-	var rhIdentity XRHIdentity
-	if err := (&echo.DefaultBinder{}).BindHeaders(c, &rhIdentity); err != nil {
-		return err
+	accountNumber, orgId, err := getAccountIdOrgId(c)
+	if err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, err.Error())
 	}
-	json.Unmarshal([]byte(rpmInput.Identity), &rpmInput)
+
+	// Request record from database
 	repoConfig := &models.RepositoryConfiguration{}
-	db.DB.First(repoConfig,
-		"uuid = ?", rpmInput.UUID,
-		"org_id = ?", rhIdentity.org_id,
-		"account_id = ?", rhIdentity.account_number,
-	)
+	if err := db.DB.First(repoConfig,
+		"uuid = ? AND org_id = ? AND account_id = ?",
+		rpmInput.UUID,
+		orgId,
+		accountNumber,
+	).Error; err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "repository_configurations:"+err.Error())
+	}
+
+	// Retrieve the linked repository record
 	repo := &models.Repository{}
-	db.DB.First(repo,
+	if err := db.DB.First(repo,
 		"refer_repo_config = ?", repoConfig.UUID,
-	)
+	).Error; err != nil {
+		return echo.NewHTTPError(http.StatusInternalServerError, "repositories:"+err.Error())
+	}
+
+	// Set pagination information
 	var total int64
 	items := make([]models.RepositoryRpm, 0)
 	page := ParsePagination(c)
 	db.DB.Where("repo_refer like ?", repo.UUID).Count(&total)
 	db.DB.Limit(page.Limit).Offset(page.Offset).Find(&items)
 
+	// Return rpm collection
 	rpms := fromRepositoryRpm2Response(items)
-	return c.JSON(200, collectionResponse(&api.RepositoryRpmCollectionResponse{Data: rpms}, c, total))
+	return c.JSON(200, setCollectionResponseMetadata(&api.RepositoryRpmCollectionResponse{Data: rpms}, c, total))
 }
 
 // fromRepositoryRpm2Response Converts the database model to our response object
