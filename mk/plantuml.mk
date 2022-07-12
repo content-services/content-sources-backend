e2279d275714e995a991200293c19dd7c76a1e37
Alejandro Visiedo <avisiedo@redhat.com>
2022-07-12 09:51:45 +0200

    use database password for the database string connection
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- mk/plantuml.mk
+++ mk/plantuml.mk
@@ -19,7 +19,7 @@ plantuml-generate: $(patsubst docs/%.puml,docs/%.svg,$(wildcard docs/*.puml)) ##
 
 .PHONY: docs/db-model.puml
 docs/db-model.puml: $(PLANTER)
-	$(GO_OUTPUT)/planter postgres://$(DATABASE_USER)@$(DATABASE_HOST)/$(DATABASE_NAME)?sslmode=disable -o $@
+	$(GO_OUTPUT)/planter postgres://$(DATABASE_USER):$(DATABASE_PASSWORD)@$(DATABASE_HOST)/$(DATABASE_NAME)?sslmode=disable -o $@
 
 $(PLANTER):
 	@{\
