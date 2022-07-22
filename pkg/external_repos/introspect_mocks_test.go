a34039dfae3186c12204a4e4ba347879da13fe11
Alejandro Visiedo <avisiedo@redhat.com>
2022-07-22 18:51:22 +0200

    pipeline:fix hints from go vet
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- pkg/external_repos/introspect_mocks_test.go
+++ pkg/external_repos/introspect_mocks_test.go
@@ -3,13 +3,11 @@ package external_repos
 import (
 	"github.com/content-services/content-sources-backend/pkg/api"
 	"github.com/content-services/yummy/pkg/yum"
-	"github.com/stretchr/testify/mock"
 )
 
 // TestIntrospect
 
 type MockRpmDao struct {
-	mock.Mock
 }
 
 func (m MockRpmDao) List(orgID string, uuidRepo string, limit int, offset int) (api.RepositoryRpmCollectionResponse, int64, error) {
