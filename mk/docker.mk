3e70f276b6038e26fcc2bd302c6aafca70c741f5
Mike Shriver <mshriver@redhat.com>
2022-08-02 10:40:06 -0400

    Refs 105: symlink containerfile for remote podman building (#67)
    symlink containerfile for remote podman building
    
    ripping this from provisioning-backend, we're seeing podman builds fail
    and suspect it is the location of the dockerfile.
    
    Ignore the linked file from commits

--- mk/docker.mk
+++ mk/docker.mk
@@ -32,7 +32,9 @@ docker-login:
 
 .PHONY: docker-build
 docker-build:  ## Build image DOCKER_IMAGE from DOCKER_DOCKERFILE using the DOCKER_CONTEXT_DIR
-	$(DOCKER) build $(DOCKER_BUILD_OPTS) -t "$(DOCKER_IMAGE)" -f $(DOCKER_DOCKERFILE) $(DOCKER_CONTEXT_DIR)
+	# spooky action at a distance
+	ln -f $(DOCKER_DOCKERFILE) Containerfile
+	$(DOCKER) build $(DOCKER_BUILD_OPTS) -t "$(DOCKER_IMAGE)" $(DOCKER_CONTEXT_DIR)
 
 .PHONY: docker-push
 docker-push:  ## Push image to remote registry
