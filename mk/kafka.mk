5d3bc80f728ebc5c781931736d6764c69a21eb07
Alejandro Visiedo <avisiedo@redhat.com>
2022-09-04 09:24:54 +0200

    makefile:fix kafka-up and kafka-down making the containers running into the same network namespace
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- mk/kafka.mk
+++ mk/kafka.mk
@@ -7,31 +7,25 @@ ZOOKEEPER_CLIENT_PORT ?= 2181
 
 .PHONY: kafka-start
 kafka-up: DOCKER_IMAGE=$(KAFKA_IMAGE)
-kafka-up:  ## Start local kafka infra
-	# $(DOCKER) volume inspect kafka &> /dev/null || $(DOCKER) volume create kafka
-	$(DOCKER) pod inspect kafka &> /dev/null \
-	|| $(DOCKER) pod create \
-	  --name kafka \
-	  --hostname kafka.test \
-	  -p "8778:8778" \
-	  -p "9092:9092" \
-	  -p $(ZOOKEEPER_CLIENT_PORT):$(ZOOKEEPER_CLIENT_PORT)
-	$(DOCKER) container inspect zookeeper &> /dev/null || $(DOCKER) run \
+kafka-up:  ## Start local kafka containers
+	$(DOCKER) container inspect kafka &> /dev/null || $(DOCKER) run \
 	  -d \
 	  --rm \
-	  --pod kafka \
 	  --name zookeeper \
 	  -e ZOOKEEPER_CLIENT_PORT=$(ZOOKEEPER_CLIENT_PORT) \
 	  -e KAFKA_OPTS="$(KAFKA_OPTS)" \
 	  -v $(PWD)/kafka/data:/tmp/zookeeper:z \
 	  -v $(PWD)/kafka/config:/tmp/config:z \
+	  -p 8778:8778 \
+	  -p 9092:9092 \
 	  "$(DOCKER_IMAGE)" \
 	  /opt/kafka/bin/zookeeper-server-start.sh /tmp/config/zookeeper.properties
 
 	$(DOCKER) container inspect kafka &> /dev/null || $(DOCKER) run \
+	  -d \
 	  --rm \
-	  --pod kafka \
 	  --name kafka \
+	  --net container:zookeeper \
 	  -e KAFKA_BROKER_ID=1 \
 	  -e KAFKA_ZOOKEEPER_CONNECT="zookeeper:2181" \
 	  -e KAFKA_ADVERTISED_LISTENERS="PLAINTEXT://kafka:9092" \
@@ -49,16 +43,18 @@ kafka-up:  ## Start local kafka infra
 #   --health-retries 10 \
 #   --health-timeout 3s \
 
-.PHONY: kafka-stop
+.PHONY: kafka-stop  ## Stop local kafka containers
 kafka-down: DOCKER_IMAGE=$(KAFKA_IMAGE)
 kafka-down:  ## Stop local kafka infra
-	! $(DOCKER) container inspect zookeeper &> /dev/null || $(DOCKER) container stop zookeeper
 	! $(DOCKER) container inspect kafka &> /dev/null || $(DOCKER) container stop kafka
-	! $(DOCKER) pod inspect kafka &> /dev/null || $(DOCKER) pod rm kafka
+	! $(DOCKER) container inspect zookeeper &> /dev/null || $(DOCKER) container stop zookeeper
 
+.PHONY: kafka-cli
+kafka-cli:  ## Open an interactive shell in kafka container
+	! $(DOCKER) container inspect kafka &> /dev/null || $(DOCKER) exec -it --workdir /opt/kafka/bin kafka /bin/bash
 
 .PHONY: kafka-build
 kafka-build: DOCKER_IMAGE=$(KAFKA_IMAGE)
-kafka-build: DOCKER_DOCKERFILE=$(PROJECTDIR)/kafka/Dockerfile
+kafka-build: DOCKER_DOCKERFILE=$(PROJECT_DIR)/kafka/Dockerfile
 kafka-build:   ## Build local kafka container image
 	$(DOCKER) build $(DOCKER_BUILD_OPTS) -t "$(DOCKER_IMAGE)" $(DOCKER_CONTEXT_DIR) -f "$(DOCKER_DOCKERFILE)"
