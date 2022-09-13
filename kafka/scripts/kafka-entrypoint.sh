237a7e8cbc1b3d6a205729d47e70f56b188c16fc
Alejandro Visiedo <avisiedo@redhat.com>
2022-09-13 10:34:20 +0200

    container:update entrypoint to generate the topics when the service is started
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- kafka/scripts/kafka-entrypoint.sh
+++ kafka/scripts/kafka-entrypoint.sh
@@ -17,8 +17,18 @@
 [ "${KAFKA_LISTENER_SECURITY_PROTOCOL_MAP}" != "" ] || KAFKA_LISTENER_SECURITY_PROTOCOL_MAP="PLAINTEXT:PLAINTEXT"
 [ "${KAFKA_INTER_BROKER_LISTENER_NAME}" != "" ] || KAFKA_INTER_BROKER_LISTENER_NAME="PLAINTEXT"
 [ "${KAFKA_OPTS}" != "" ] || KAFKA_OPTS="-javaagent:/usr/jolokia/agents/jolokia-jvm.jar=host=0.0.0.0"
+[ "${KAFKA_TOPIC}" != "" ] || KAFKA_TOPIC="repos.created"
 
 # TODO Handle the variables to generate the configuration or execute the necessary commands
 #      to get the container running as we need
+
+function create_topics {
+    while ! /opt/kafka/bin/kafka-topics.sh --zookeeper localhost:2181/kafka --create --topic "${KAFKA_TOPIC}" --replication-factor 1 --partitions 3; do
+        sleep 1
+    done
+    /opt/kafka/bin/kafka-topics.sh --zookeeper localhost:2181/kafka --topic "${KAFKA_TOPIC}" --describe
+}
+
+nohup create_topics &
 
 exec "${KAFKA_HOME}/bin/kafka-server-start.sh" /tmp/config/server.properties # "${KAFKA_OPTS}"
