a38b74d61dc1ec0298aa247041b9bf23fdf86cc9
Ryan Verdile <ryanverdile@gmail.com>
2022-09-20 14:06:34 -0400

    Fixes 185: Save revision properly

--- pkg/dao/repositories.go
+++ pkg/dao/repositories.go
@@ -12,7 +12,6 @@ type Repository struct {
 	UUID                         string
 	URL                          string
 	Revision                     string
-	Public                       bool
 	LastIntrospectionTime        *time.Time
 	LastIntrospectionSuccessTime *time.Time
 	LastIntrospectionUpdateTime  *time.Time
@@ -32,22 +31,26 @@ type repositoryDaoImpl struct {
 
 func (p repositoryDaoImpl) FetchForUrl(url string) (error, Repository) {
 	repo := models.Repository{}
+	internalRepo := Repository{}
 	result := p.db.Where("URL = ?", url).Order("url asc").First(&repo)
 	if result.Error != nil {
 		return result.Error, Repository{}
 	}
-	return nil, modelToInternal(repo)
+	modelToInternal(repo, &internalRepo)
+	return nil, internalRepo
 }
 
 func (p repositoryDaoImpl) List() (error, []Repository) {
 	var dbRepos []models.Repository
 	var repos []Repository
+	var repo Repository
 	result := p.db.Find(&dbRepos)
 	if result.Error != nil {
 		return result.Error, repos
 	}
 	for i := 0; i < len(dbRepos); i++ {
-		repos = append(repos, modelToInternal(dbRepos[i]))
+		modelToInternal(dbRepos[i], &repo)
+		repos = append(repos, repo)
 	}
 	return nil, repos
 }
@@ -60,9 +63,9 @@ func (p repositoryDaoImpl) Update(repoIn Repository) error {
 		return result.Error
 	}
 
-	repo := internalToModel(repoIn)
+	internalToModel(repoIn, &dbRepo)
 
-	result = p.db.Model(&repo).Updates(repo.MapForUpdate())
+	result = p.db.Model(&dbRepo).Updates(dbRepo.MapForUpdate())
 	if result.Error != nil {
 		return result.Error
 	}
@@ -71,31 +74,38 @@ func (p repositoryDaoImpl) Update(repoIn Repository) error {
 }
 
 // modelToInternal returns internal Repository with fields of model
-func modelToInternal(model models.Repository) Repository {
-	var internal Repository
+func modelToInternal(model models.Repository, internal *Repository) {
 	internal.UUID = model.UUID
 	internal.URL = model.URL
-	internal.Public = model.Public
 	internal.Revision = model.Revision
 	internal.LastIntrospectionError = model.LastIntrospectionError
 	internal.LastIntrospectionTime = model.LastIntrospectionTime
 	internal.LastIntrospectionUpdateTime = model.LastIntrospectionUpdateTime
 	internal.LastIntrospectionSuccessTime = model.LastIntrospectionSuccessTime
 	internal.Status = model.Status
-	return internal
 }
 
-// internalToModel returns model Repository with fields of internal
-func internalToModel(internal Repository) models.Repository {
-	var model models.Repository
-	model.UUID = internal.UUID
-	model.URL = internal.URL
-	model.Public = internal.Public
-	model.Revision = internal.Revision
-	model.LastIntrospectionError = internal.LastIntrospectionError
-	model.LastIntrospectionTime = internal.LastIntrospectionTime
-	model.LastIntrospectionUpdateTime = internal.LastIntrospectionUpdateTime
-	model.LastIntrospectionSuccessTime = internal.LastIntrospectionSuccessTime
-	model.Status = internal.Status
-	return model
+// internalToModel updates model Repository with non-zero fields of internal
+func internalToModel(internal Repository, model *models.Repository) {
+	if internal.URL != "" {
+		model.URL = internal.URL
+	}
+	if internal.Revision != "" {
+		model.Revision = internal.Revision
+	}
+	if internal.LastIntrospectionError != nil {
+		model.LastIntrospectionError = internal.LastIntrospectionError
+	}
+	if internal.LastIntrospectionTime != nil {
+		model.LastIntrospectionTime = internal.LastIntrospectionTime
+	}
+	if internal.LastIntrospectionUpdateTime != nil {
+		model.LastIntrospectionUpdateTime = internal.LastIntrospectionUpdateTime
+	}
+	if internal.LastIntrospectionSuccessTime != nil {
+		model.LastIntrospectionSuccessTime = internal.LastIntrospectionSuccessTime
+	}
+	if internal.Status != "" {
+		model.Status = internal.Status
+	}
 }
