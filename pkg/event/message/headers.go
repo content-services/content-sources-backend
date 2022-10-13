088d3a4bd81be49cd20de46fd38ba9474c8647e8
Alejandro Visiedo <avisiedo@redhat.com>
2022-10-13 18:29:17 +0200

    clean-up:remove not used code for the headers
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- pkg/event/message/headers.go
+++ pkg/event/message/headers.go
@@ -1,46 +1,11 @@
 package message
 
-import (
-	"fmt"
-
-	"github.com/confluentinc/confluent-kafka-go/kafka"
-)
-
 type EventHeaderKey string
 
 const (
-	HdrType                 EventHeaderKey = "Type"
+	HdrType           EventHeaderKey = "Type"
+	HdrTypeIntrospect string         = "Introspect"
+
 	HdrXRhIdentity          EventHeaderKey = "X-Rh-Identity"
 	HdrXRhInsightsRequestId EventHeaderKey = "X-Rh-Insights-Request-Id"
 )
-
-type Header map[EventHeaderKey]string
-
-const (
-	HdrTypeIntrospect = "Introspect"
-)
-
-func (h *Header) Set(key EventHeaderKey, value string) {
-	var m map[EventHeaderKey]string = *h
-	m[key] = value
-}
-
-func (h *Header) Get(key EventHeaderKey) (string, error) {
-	var m map[EventHeaderKey]string = *h
-	if value, ok := m[key]; ok {
-		return value, nil
-	}
-	return "", fmt.Errorf("key '%s' not found", string(key))
-}
-
-func (h *Header) GetAll() []kafka.Header {
-	var m map[EventHeaderKey]string = *h
-	var output []kafka.Header
-	for key, value := range m {
-		output = append(output, kafka.Header{
-			Key:   string(key),
-			Value: []byte(value),
-		})
-	}
-	return output
-}
