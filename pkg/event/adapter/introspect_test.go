5719b6b8054bb33b3f0c69cb86e5186ca69891da
Alejandro Visiedo <avisiedo@redhat.com>
2022-10-18 21:22:33 +0200

    test:update event adapter unit test for increasing coverage
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- pkg/event/adapter/introspect_test.go
+++ pkg/event/adapter/introspect_test.go
@@ -69,4 +69,41 @@ func TestFromRepositoryBulkCreateResponse(t *testing.T) {
 		Given    *api.RepositoryBulkCreateResponse
 		Expected TestCaseExpected
 	}
+
+	testCases := []TestCase{
+		{
+			Given: nil,
+			Expected: TestCaseExpected{
+				err: fmt.Errorf("repositoryBulkCreateResponse cannot be nil"),
+				msg: nil,
+			},
+		},
+		{
+			Given: &api.RepositoryBulkCreateResponse{
+				Repository: &api.RepositoryResponse{
+					UUID: "6742a4c0-0fe5-4abc-9037-bfbe57d3bcb5",
+					URL:  "https://my-awesome-repository.test",
+				},
+			},
+			Expected: TestCaseExpected{
+				err: nil,
+				msg: &message.IntrospectRequestMessage{
+					Uuid: "6742a4c0-0fe5-4abc-9037-bfbe57d3bcb5",
+					Url:  "https://my-awesome-repository.test",
+				},
+			},
+		},
+	}
+
+	for _, testCase := range testCases {
+		result, err := NewIntrospect().FromRepositoryBulkCreateResponse(testCase.Given)
+		if testCase.Expected.err != nil {
+			require.Error(t, err)
+			assert.Equal(t, testCase.Expected.err.Error(), err.Error())
+			assert.Equal(t, testCase.Expected.msg, result)
+		} else {
+			assert.NoError(t, err)
+			assert.Equal(t, testCase.Expected.msg, result)
+		}
+	}
 }
