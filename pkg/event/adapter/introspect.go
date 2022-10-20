ca94f9f3b482df4adaf5bae905a236258db09356
Alejandro <alejandro.visiedo@gmail.com>
2022-10-20 12:13:53 +0200

    Update pkg/event/adapter/introspect.go
    Co-authored-by: swadeley <swadeley@redhat.com>

--- pkg/event/adapter/introspect.go
+++ pkg/event/adapter/introspect.go
@@ -38,7 +38,7 @@ func (a IntrospectRequest) FromRepositoryResponse(repositoryResponse *api.Reposi
 }
 
 // FromRepositoryBulkCreateResponse convert an api.RepositoryBulkCreateResponse into a message.IntrospectRequestMessage
-// Return a message.IntrospectRequestMessage and nil error when everythin goes well, else
+// Return a message.IntrospectRequestMessage and nil error when everything goes well, else
 //   a nil message and an error filled with the root cause.
 func (a IntrospectRequest) FromRepositoryBulkCreateResponse(repositoryBulkCreateResponse *api.RepositoryBulkCreateResponse) (*message.IntrospectRequestMessage, error) {
 	if repositoryBulkCreateResponse == nil {
