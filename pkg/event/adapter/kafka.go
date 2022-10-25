e2250a0a0e52aa9e2858fe9583269698ff07a00a
Alejandro Visiedo <avisiedo@redhat.com>
2022-10-25 14:58:50 +0200

    doc:move documentation to the specific implementation to improve readability of the interface
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- pkg/event/adapter/kafka.go
+++ pkg/event/adapter/kafka.go
@@ -12,12 +12,6 @@ import (
 // KafkaHeaders is the adapter interface to translate to kafka.Header slice
 //   which is used to compose a kafka message.
 type KafkaHeaders interface {
-	// FromEchoContext translate from an echo.Context to []kafka.Header
-	// ctx is the echo.Context from an http handler.
-	// event is an additional type to identify exactly the schema which match
-	//   with the kafka message.
-	// Return headers a slice of kafka.Header and nil error when success, else
-	//   an error reference filled and an empty slice of kafka.Header.
 	FromEchoContext(ctx echo.Context, event string) (headers []kafka.Header, err error)
 }
 
@@ -39,6 +33,12 @@ func getEchoHeader(ctx echo.Context, key string, defvalues []string) []string {
 	return defvalues
 }
 
+// FromEchoContext translate from an echo.Context to []kafka.Header
+// ctx is the echo.Context from an http handler.
+// event is an additional type to identify exactly the schema which match
+//   with the kafka message.
+// Return headers a slice of kafka.Header and nil error when success, else
+//   an error reference filled and an empty slice of kafka.Header.
 func (a KafkaAdapter) FromEchoContext(ctx echo.Context, event string) (headers []kafka.Header, err error) {
 	if ctx == nil {
 		return []kafka.Header{}, fmt.Errorf("ctx cannot be nil")
