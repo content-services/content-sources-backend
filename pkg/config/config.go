82157ebfd82c1427aaf5692c8365bdccb4786836
Alejandro Visiedo <avisiedo@redhat.com>
2022-11-23 22:46:13 +0100

    rbac:add rbac middleware to the service
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -129,7 +129,7 @@ func setDefaults(v *viper.Viper) {
 	v.SetDefault("metrics.path", "/metrics")
 	v.SetDefault("metrics.port", 9000)
 	v.SetDefault("clients.rbac_enabled", true)
-	v.SetDefault("clients.rbac_base_url", "https://rbac-service:8080/api/rbac/v1")
+	v.SetDefault("clients.rbac_base_url", "https://rbac-service:8000/api/rbac/v1")
 
 	v.SetDefault("cloudwatch.region", "")
 	v.SetDefault("cloudwatch.group", "")
