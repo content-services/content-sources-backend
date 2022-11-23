fee53eeeec7bd2f564654d90a92233415843143b
Alejandro Visiedo <avisiedo@redhat.com>
2022-11-23 22:40:14 +0100

    mock:add makefile rules to start / stop
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

---
+++ mk/mock.mk
@@ -0,0 +1,22 @@
+##
+# This file make easier to mock external clients by providing
+# a static content for it.
+##
+
+MOCK_PID := $(PROJECT_DIR)/.mock
+MOCK_PORT := 8800
+
+.PHONY: mock-up
+mock-up: $(PROJECT_DIR)/release/mock ## Start mock service
+	[ ! -e "$(MOCK_PID)" ] || ( echo "mock service is currently up"; exit 1 )
+	"$(PROJECT_DIR)/release/mock" --directory "$(PROJECT_DIR)/test/data/mock" 8800 &> mock.logs & echo $$! > "$(MOCK_PID)"
+
+.PHONY: mock-down
+mock-down: ## Stop mock services
+	[ -e "$(MOCK_PID)" ] || ( echo "mock service is not up currently"; exit 1 )
+	/usr/bin/kill -SIGTERM "$$(<"$(MOCK_PID)")"
+	[ ! -e "$(MOCK_PID)" ] || rm -f "$(MOCK_PID)"
+
+.PHONY: mock-clean
+mock-clean:  ## Clean the mock service
+	[ ! -e "$(MOCK_PID)" ] || rm -f "$(MOCK_PID)"
