1a96221de18c6303399819a39698f0c529ac9cad
Alejandro Visiedo <avisiedo@redhat.com>
2023-01-03 12:58:52 +0100

    test:update suite test wrapped by the custom metric suite test to start a new transaction for every unit test
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- pkg/dao/suite_test.go
+++ pkg/dao/suite_test.go
@@ -1,14 +1,19 @@
 package dao
 
 import (
+	"log"
+	"os"
 	"time"
 
 	"github.com/content-services/content-sources-backend/pkg/config"
+	"github.com/content-services/content-sources-backend/pkg/db"
 	"github.com/content-services/content-sources-backend/pkg/models"
 	"github.com/content-services/content-sources-backend/pkg/seeds"
+	"github.com/google/uuid"
 	"github.com/lib/pq"
 	"github.com/stretchr/testify/suite"
 	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
 )
 
 type DaoSuite struct {
@@ -54,6 +59,7 @@ var repoPrivateTest = models.Repository{
 
 var repoConfigTest1 = models.RepositoryConfiguration{
 	Base: models.Base{
+		UUID:      uuid.NewString(),
 		CreatedAt: time.Now(),
 		UpdatedAt: time.Now(),
 	},
@@ -99,4 +105,22 @@ func (s *DaoSuite) TearDownTest() {
 	//Rollback and reset db.DB
 	s.tx.Rollback()
 	s.db.SkipDefaultTransaction = s.skipDefaultTransactionOld
+}
+
+func (s *DaoSuite) SetupTest() {
+	if db.DB == nil {
+		if err := db.Connect(); err != nil {
+			s.FailNow(err.Error())
+		}
+	}
+	s.skipDefaultTransactionOld = db.DB.SkipDefaultTransaction
+	s.db = db.DB.Session(&gorm.Session{
+		SkipDefaultTransaction: false,
+		Logger: logger.New(
+			log.New(os.Stderr, "\r\n", log.LstdFlags),
+			logger.Config{
+				LogLevel: logger.Info,
+			}),
+	})
+	s.tx = s.db.Begin()
 }
