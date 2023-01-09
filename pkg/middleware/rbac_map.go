dec1e193510305e4037be8abb0f6867661969b39
Alejandro Visiedo <avisiedo@redhat.com>
2023-01-09 13:46:33 +0100

    rbac:update proposal for permissions mapping file format
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- pkg/middleware/rbac_map.go
+++ pkg/middleware/rbac_map.go
@@ -4,8 +4,19 @@ import "net/http"
 
 // TODO Add this information from an external file and load it, so it can be updated without change code
 // - Use embed to inject the file into the code.
-// - Define a yaml file for this (easier to edit than a json file)
-//
+// - Define a yaml file for this (easier to edit than a json file).
+// An example for that file could be something like
+/*
+permissionMap:
+- method: GET
+  route: popular_repositories
+  permission: repository
+  verb: read
+- method: GET
+  route: repositories
+  permission: repositories
+  verb: read
+*/
 
 // See: https://redocly.github.io/redoc/?url=https://raw.githubusercontent.com/content-services/content-sources-backend/main/api/openapi.json
 var ServicePermissions *PermissionsMap = NewPermissionsMap().
