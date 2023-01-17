bc45597b61777c37d170ce917decdfad4ce80d92
Alejandro Visiedo <avisiedo@redhat.com>
2023-01-18 00:11:14 +0100

    rbac:fix some information lost during rebase for the routing package
    Signed-off-by: Alejandro Visiedo <avisiedo@redhat.com>

--- pkg/router/route.go
+++ pkg/router/route.go
@@ -10,6 +10,7 @@ import (
 	"github.com/content-services/content-sources-backend/pkg/middleware"
 	"github.com/labstack/echo/v4"
 	echo_middleware "github.com/labstack/echo/v4/middleware"
+	echo_log "github.com/labstack/gommon/log"
 	"github.com/redhatinsights/platform-go-middlewares/identity"
 	"github.com/rs/zerolog/log"
 	"github.com/ziflex/lecho/v3"
@@ -17,32 +18,36 @@ import (
 
 func ConfigureEcho(allRoutes bool) *echo.Echo {
 	e := echo.New()
+	// Add global middlewares
 	echoLogger := lecho.From(log.Logger,
 		lecho.WithTimestamp(),
 		lecho.WithCaller(),
+		lecho.WithLevel(echo_log.INFO),
 	)
-	e.Use(echo_middleware.AddTrailingSlash(), echo_middleware.Logger())
 	e.Use(echo_middleware.RequestIDWithConfig(echo_middleware.RequestIDConfig{
 		TargetHeader: "x-rh-insights-request-id",
 	}))
 	e.Use(lecho.Middleware(lecho.Config{
 		Logger:       echoLogger,
 		RequestIDKey: "x-rh-insights-request-id",
+		Skipper:      config.SkipLogging,
 	}))
 
-	// Liveness and readiness are set up before security checks
+	// Add routes
 	handler.RegisterPing(e)
-
 	if allRoutes {
 		handler.RegisterRoutes(e)
 	}
 
+	// Set error handler
 	e.HTTPErrorHandler = config.CustomHTTPErrorHandler
 	return e
 }
 
 func ConfigureEchoWithMetrics(metrics *instrumentation.Metrics) *echo.Echo {
 	e := ConfigureEcho(true)
+
+	// Add additional global middlewares
 	e.Use(middleware.CreateMetricsMiddleware(metrics))
 	e.Use(middleware.WrapMiddlewareWithSkipper(identity.EnforceIdentity, middleware.SkipLiveness))
 	if config.Get().Clients.RbacEnabled {
