e3b22eb6e8441bef7e498498eea837c244947507
Justin Sherrill <jsherril@redhat.com>
2023-01-18 14:20:34 -0500

    add project name for pulp deployment

--- mk/pulp.mk
+++ mk/pulp.mk
@@ -3,8 +3,8 @@ PULP_COMPOSE_FILES ?= "compose_files/pulp/pulp-oci-images/images/compose/docker-
 PULP_COMPOSE_OPTIONS=PULP_POSTGRES_PATH="pulp_db" PULP_STORAGE_PATH="pulp_storage"
 
 ifeq ($(DEPLOY_PULP),true)
-PULP_COMPOSE_COMMAND=$(PULP_COMPOSE_OPTIONS) $(DOCKER)-compose  -f $(PULP_COMPOSE_FILES) up --detach
-PULP_COMPOSE_DOWN_COMMAND=$(PULP_COMPOSE_OPTIONS) $(DOCKER)-compose  -f $(PULP_COMPOSE_FILES) down
+PULP_COMPOSE_COMMAND=$(PULP_COMPOSE_OPTIONS) $(DOCKER)-compose --project-name=$(COMPOSE_PROJECT_NAME)  -f $(PULP_COMPOSE_FILES) up --detach
+PULP_COMPOSE_DOWN_COMMAND=$(PULP_COMPOSE_OPTIONS) $(DOCKER)-compose --project-name=$(COMPOSE_PROJECT_NAME) -f $(PULP_COMPOSE_FILES) down
 else
 PULP_COMPOSE_COMMAND=echo "Skipping pulp deploy, set DEPLOY_PULP=true to deploy"
 PULP_COMPOSE_DOWN_COMMAND=true
