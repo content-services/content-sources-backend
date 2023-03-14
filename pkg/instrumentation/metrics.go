57d57d93919d6232324234f087bf794e5e98c88b
Justin Sherrill <jsherril@redhat.com>
2023-03-14 08:57:07 -0400

    Refs 1203: Rename kafka metrics
    So that we can reuse them when we switch to a newer
    tasking system

--- pkg/instrumentation/metrics.go
+++ pkg/instrumentation/metrics.go
@@ -17,8 +17,8 @@ const (
 	PublicRepositories36HourIntrospectionTotal     = "public_repositories_36_hour_introspection_total"
 	PublicRepositoriesWithFailedIntrospectionTotal = "public_repositories_with_failed_introspection_total"
 	CustomRepositories36HourIntrospectionTotal     = "custom_repositories_36_hour_introspection_total"
-	KafkaMessageLatency                            = "kafka_message_latency"
-	KafkaMessageResultTotal                        = "kafka_message_result_total"
+	MessageLatency                                 = "message_latency"
+	MessageResultTotal                             = "message_result_total"
 	OrgTotal                                       = "org_total"
 )
 
@@ -31,8 +31,8 @@ type Metrics struct {
 	PublicRepositories36HourIntrospectionTotal     prometheus.GaugeVec
 	PublicRepositoriesWithFailedIntrospectionTotal prometheus.Gauge
 	CustomRepositories36HourIntrospectionTotal     prometheus.GaugeVec
-	KafkaMessageResultTotal                        prometheus.CounterVec
-	KafkaMessageLatency                            prometheus.Histogram
+	MessageResultTotal                             prometheus.CounterVec
+	MessageLatency                                 prometheus.Histogram
 	OrgTotal                                       prometheus.Gauge
 	reg                                            *prometheus.Registry
 }
@@ -52,16 +52,16 @@ func NewMetrics(reg *prometheus.Registry) *Metrics {
 			Buckets:   prometheus.DefBuckets,
 		}, []string{"status", "method", "path"}),
 
-		KafkaMessageLatency: promauto.With(reg).NewHistogram(prometheus.HistogramOpts{
+		MessageLatency: promauto.With(reg).NewHistogram(prometheus.HistogramOpts{
 			Namespace: NameSpace,
-			Name:      KafkaMessageLatency,
-			Help:      "Time to pickup kafka messages",
+			Name:      MessageLatency,
+			Help:      "Time to pickup task messages",
 			Buckets:   prometheus.DefBuckets,
 		}),
-		KafkaMessageResultTotal: *promauto.With(reg).NewCounterVec(prometheus.CounterOpts{
+		MessageResultTotal: *promauto.With(reg).NewCounterVec(prometheus.CounterOpts{
 			Namespace:   NameSpace,
-			Name:        KafkaMessageResultTotal,
-			Help:        "Result of kafka messages",
+			Name:        MessageResultTotal,
+			Help:        "Result of task messages",
 			ConstLabels: nil,
 		}, []string{"state"}),
 		RepositoriesTotal: promauto.With(reg).NewGauge(prometheus.GaugeOpts{
@@ -101,18 +101,18 @@ func NewMetrics(reg *prometheus.Registry) *Metrics {
 	return metrics
 }
 
-func (m *Metrics) RecordKafkaMessageResult(success bool) {
+func (m *Metrics) RecordMessageResult(success bool) {
 	status := "failed"
 	if success {
 		status = "success"
 	}
 	if m != nil {
-		m.KafkaMessageResultTotal.With(prometheus.Labels{"state": status}).Inc()
+		m.MessageResultTotal.With(prometheus.Labels{"state": status}).Inc()
 	}
 }
-func (m *Metrics) RecordKafkaLatency(msgTime time.Time) {
+func (m *Metrics) RecordMessageLatency(msgTime time.Time) {
 	diff := time.Since(msgTime)
-	m.KafkaMessageLatency.Observe(diff.Seconds())
+	m.MessageLatency.Observe(diff.Seconds())
 }
 
 func (m Metrics) Registry() *prometheus.Registry {
