6d57dfc9642074522125ad631aacd124833046bc
Andrew Dewar <andrewgdewar@gmail.com>
2023-06-05 10:08:46 -0600

    Fix test

--- pkg/notifications/client_test.go
+++ pkg/notifications/client_test.go
@@ -10,14 +10,14 @@ func TestSetEmptyToNil(t *testing.T) {
 	testedVariables := []string{"someValue", "otherValue", "s"}
 
 	for i := 0; i < len(testedVariables); i++ {
-		result := SetEmptyToNil(&testedVariables[i])
+		result := SetEmptyToNil(testedVariables[i])
 		assert.NotNil(t, result)
 	}
 
 	testedNilVariables := []string{""}
 
 	for i := 0; i < len(testedNilVariables); i++ {
-		result := SetEmptyToNil(&testedNilVariables[i])
+		result := SetEmptyToNil(testedNilVariables[i])
 		assert.Nil(t, result)
 	}
 }
