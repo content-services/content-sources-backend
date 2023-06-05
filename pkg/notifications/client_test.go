2a7e282880b5e5cda9fbd1588821313c78c09118
Andrew Dewar <andrewgdewar@gmail.com>
2023-06-05 09:51:04 -0600

    Fixes 602: Update schema output and dataSchema typo

---
+++ pkg/notifications/client_test.go
@@ -0,0 +1,23 @@
+package notifications
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSetEmptyToNil(t *testing.T) {
+	testedVariables := []string{"someValue", "otherValue", "s"}
+
+	for i := 0; i < len(testedVariables); i++ {
+		result := SetEmptyToNil(&testedVariables[i])
+		assert.NotNil(t, result)
+	}
+
+	testedNilVariables := []string{""}
+
+	for i := 0; i < len(testedNilVariables); i++ {
+		result := SetEmptyToNil(&testedNilVariables[i])
+		assert.Nil(t, result)
+	}
+}
