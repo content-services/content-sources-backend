8374c0cd66c07e0d85e75726862ff13f257c88f2
xbhouse <bhouse@redhat.com>
2023-12-13 15:05:20 -0500

    Revert "Fixes 3016: Reject requests with org id of -1 (#499)"
    This reverts commit 2802ea28c793aec866a79f89115d06e75adc2729.

--- pkg/router/route.go
+++ pkg/router/route.go
@@ -18,7 +18,6 @@ import (
 
 func ConfigureEcho(allRoutes bool) *echo.Echo {
 	e := echo.New()
-
 	// Add global middlewares
 	echoLogger := lecho.From(log.Logger,
 		lecho.WithTimestamp(),
@@ -34,8 +33,6 @@ func ConfigureEcho(allRoutes bool) *echo.Echo {
 		RequestIDKey:    config.RequestIdLoggingKey,
 		Skipper:         config.SkipLogging,
 	}))
-	e.Use(middleware.WrapMiddlewareWithSkipper(identity.EnforceIdentity, middleware.SkipAuth))
-	e.Use(middleware.EnforceOrgId)
 	e.Use(middleware.EnforceJSONContentType)
 
 	// Add routes
@@ -54,6 +51,7 @@ func ConfigureEchoWithMetrics(metrics *instrumentation.Metrics) *echo.Echo {
 
 	// Add additional global middlewares
 	e.Use(middleware.CreateMetricsMiddleware(metrics))
+	e.Use(middleware.WrapMiddlewareWithSkipper(identity.EnforceIdentity, middleware.SkipAuth))
 	if config.Get().Clients.RbacEnabled {
 		rbacBaseUrl := config.Get().Clients.RbacBaseUrl
 		rbacTimeout := time.Duration(int64(config.Get().Clients.RbacTimeout) * int64(time.Second))
