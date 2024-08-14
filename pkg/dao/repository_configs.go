56823bde2b67f7f3269cc664912279698e95575d
rverdile <ryanverdile@gmail.com>
2024-08-14 15:18:27 -0400

    set empty list for repo response

--- pkg/dao/repository_configs.go
+++ pkg/dao/repository_configs.go
@@ -931,6 +931,8 @@ func ModelToApiFields(repoConfig models.RepositoryConfiguration, apiRepo *api.Re
 			OrgId:          repoConfig.LastSnapshotTask.OrgId,
 			RepoConfigUUID: repoConfig.UUID,
 			RepoConfigName: repoConfig.Name,
+			Dependencies:   []string{},
+			Dependents:     []string{},
 		}
 		if repoConfig.LastSnapshotTask.Started != nil {
 			apiRepo.LastSnapshotTask.CreatedAt = repoConfig.LastSnapshotTask.Started.Format(time.RFC3339)
