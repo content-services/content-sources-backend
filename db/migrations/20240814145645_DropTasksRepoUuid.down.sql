1b4679e25c91ac59e5a0089015c46d0c9dc04193
Justin Sherrill <jsherril@redhat.com>
2024-08-14 15:12:35 -0400

    Refs 4313: drop tasks repo_uuid

---
+++ db/migrations/20240814145645_DropTasksRepoUuid.down.sql
@@ -0,0 +1,17 @@
+BEGIN;
+
+alter table tasks add column if not exists repository_uuid UUID;
+
+CREATE OR REPLACE VIEW ready_tasks AS
+SELECT *
+FROM tasks
+WHERE started_at IS NULL
+  AND (status != 'canceled' OR status is null)
+  AND id NOT IN (
+    SELECT task_id
+    FROM task_dependencies JOIN tasks ON dependency_id = id
+    WHERE finished_at IS NULL
+)
+ORDER BY priority DESC, queued_at ASC;
+
+COMMIT;
