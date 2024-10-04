96d6514585a894c0d76810cc97adc0e1f8d1bb4e
Bryttanie House <bhouse@redhat.com>
2024-10-04 08:42:42 -0400

    Fixes 4151: remove support for yyyymmdd date formats

--- pkg/dao/snapshots_test.go
+++ pkg/dao/snapshots_test.go
@@ -480,7 +480,7 @@ func (s *SnapshotsSuite) TestFetchSnapshotsByDateAndRepository() {
 
 	request := api.ListSnapshotByDateRequest{}
 
-	request.Date = api.Date(second.Base.CreatedAt)
+	request.Date = second.Base.CreatedAt
 
 	request.RepositoryUUIDS = []string{repoConfig.UUID}
 
@@ -515,7 +515,7 @@ func (s *SnapshotsSuite) TestFetchSnapshotsByDateAndRepositoryMulti() {
 	target3 := s.createSnapshotAtSpecifiedTime(redhatRepo, baseTime.Add(-time.Hour*100)) // Closest to Target Date
 
 	request := api.ListSnapshotByDateRequest{}
-	request.Date = api.Date(target1.Base.CreatedAt)
+	request.Date = target1.Base.CreatedAt
 
 	// Intentionally not found ID
 	randomUUID, _ := uuid2.NewUUID()
