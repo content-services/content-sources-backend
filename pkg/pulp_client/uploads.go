869620e3fb5e0802ccce5869baa17199d1ce4e49
rverdile <ryanverdile@gmail.com>
2024-10-17 15:03:20 -0400

    set default response code

--- pkg/pulp_client/uploads.go
+++ pkg/pulp_client/uploads.go
@@ -2,6 +2,7 @@ package pulp_client
 
 import (
 	"context"
+	"net/http"
 	"os"
 
 	zest "github.com/content-services/zest/release/v2024"
@@ -14,23 +15,26 @@ func (r *pulpDaoImpl) CreateUpload(ctx context.Context, size int64) (*zest.Uploa
 	if err != nil {
 		return nil, 0, err
 	}
+
+	statusCode := http.StatusInternalServerError
 	upload := zest.Upload{}
 	upload.Size = size
 	readResp, httpResp, err := client.UploadsAPI.UploadsCreate(ctx, r.domainName).Upload(upload).Execute()
 	if httpResp != nil {
+		statusCode = httpResp.StatusCode
 		defer httpResp.Body.Close()
 	}
 	if err != nil {
-		return nil, httpResp.StatusCode, errorWithResponseBody("creating upload", httpResp, err)
+		return nil, statusCode, errorWithResponseBody("creating upload", httpResp, err)
 	}
-	return readResp, httpResp.StatusCode, nil
+	return readResp, statusCode, nil
 }
 
 // UploadChunk Uploads a chunk for an upload
 func (r *pulpDaoImpl) UploadChunk(ctx context.Context, uploadHref string, contentRange string, file *os.File, sha256 string) (*zest.UploadResponse, int, error) {
-	var statusCode int
-
 	ctx, client := getZestClient(ctx)
+	statusCode := http.StatusInternalServerError
+
 	readResp, httpResp, err := client.UploadsAPI.UploadsUpdate(ctx, uploadHref).ContentRange(contentRange).File(file).Sha256(sha256).Execute()
 	if httpResp != nil {
 		statusCode = httpResp.StatusCode
@@ -44,10 +48,10 @@ func (r *pulpDaoImpl) UploadChunk(ctx context.Context, uploadHref string, conten
 
 // FinishUpload Finishes an upload
 func (r *pulpDaoImpl) FinishUpload(ctx context.Context, uploadHref string, sha256 string) (*zest.AsyncOperationResponse, int, error) {
-	var statusCode int
 	ctx, client := getZestClient(ctx)
 	uploadCommit := zest.UploadCommit{}
 	uploadCommit.Sha256 = sha256
+	statusCode := http.StatusInternalServerError
 
 	readResp, httpResp, err := client.UploadsAPI.UploadsCommit(ctx, uploadHref).UploadCommit(uploadCommit).Execute()
 	if httpResp != nil {
