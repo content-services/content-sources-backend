ea0e24cd5c25653c35c97ff110701a964bd0e9ff
Andrew Dewar <andrewgdewar@gmail.com>
2024-12-05 15:13:12 -0700

    Migrate module_streams

---
+++ pkg/api/module_streams.go
@@ -0,0 +1,37 @@
+package api
+
+type SearchModuleStreamsRequest struct {
+	UUIDs    []string `json:"uuids" validate:"required"`     // List of snapshot UUIDs to search
+	RpmNames []string `json:"rpm_names" validate:"required"` // List of rpm names to search
+	SortBy   string   `json:"sort_by"`                       // SortBy sets the sort order of the result
+	Search   string   `json:"search"`                        // Search string to search rpm names
+}
+
+type Stream struct {
+	Name        string              // Name of the module
+	Stream      string              // Module stream version
+	Context     string              // Context of the module
+	Arch        string              // The Architecture of the rpm
+	Version     string              // The version of the rpm
+	Description string              // Module description
+	Profiles    map[string][]string // Module profile data
+}
+
+type SearchModuleStreams struct {
+	ModuleName string   `json:"module_name"` // Module name
+	Streams    []Stream `json:"streams"`     // A list of stream related information for the module
+}
+
+type SearchModuleStreamsCollectionResponse struct {
+	Data  []SearchModuleStreams `json:"data"`  // Requested Data
+	Meta  ResponseMetadata      `json:"meta"`  // Metadata about the request
+	Links Links                 `json:"links"` // Links to other pages of results
+}
+
+// SetMetadata Map metadata to the collection.
+// meta Metadata about the request.
+// links Links to other pages of results.
+func (r *SearchModuleStreamsCollectionResponse) SetMetadata(meta ResponseMetadata, links Links) {
+	r.Meta = meta
+	r.Links = links
+}
