ddfb1e610c0175a46abc8d82b6f9553602b73c48
adewar <andrewgdewar@gmail.com>
2025-02-05 11:49:49 -0700

    Update example config to allow all-features by default

--- _playwright-tests/tests/API/CheckGetFeatures.spec.ts
+++ _playwright-tests/tests/API/CheckGetFeatures.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('Content > GetRepositories API', async ({ request }) => {
-    const result = await request.get('/api/content-sources/v1/repositories/');
+test('Content > GetFeatures API', async ({ request }) => {
+    const result = await request.get('/api/content-sources/v1/features/');
     expect(result.status()).toBe(200);
 });
