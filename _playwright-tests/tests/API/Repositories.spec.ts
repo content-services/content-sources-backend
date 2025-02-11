5a4d96eb0c66b3a82ab6ff4b2825c65cf1cd7676
rverdile <ryanverdile@gmail.com>
2025-02-11 16:04:14 -0500

    HMS-5430: add playwright introspect test

---
+++ _playwright-tests/tests/API/Repositories.spec.ts
@@ -0,0 +1,21 @@
+import {test} from "./base_client";
+import {RepositoriesApi, GetRepositoryRequest, ApiRepositoryResponse, PopularRepositoriesApi, TasksApi} from "./client";
+import {expect} from "@playwright/test";
+import {poll} from "../helpers/apiHelpers";
+
+test('Content > Verify repository introspection', async ({ client }) => {
+  const repo = await new RepositoriesApi(client).createRepository({apiRepositoryRequest: {name: "test-repository", url: "https://rverdile.fedorapeople.org/dummy-repos/modules/repo1/" }});
+    expect(repo.name).toBe("test-repository");
+
+    await test.step("wait for introspection to be completed", async () => {
+        let getRepository = () => new RepositoriesApi(client).getRepository(<GetRepositoryRequest>{uuid: repo.uuid?.toString()})
+        let waitWhilePending = (resp: ApiRepositoryResponse) => resp.status === "Pending"
+        let resp = await poll(getRepository, waitWhilePending, 10)
+        expect(resp.status).toBe("Valid")
+    })
+
+    await test.step("delete repository", async () => {
+        const resp = await new RepositoriesApi(client).deleteRepositoryRaw(<GetRepositoryRequest>{uuid: repo.uuid?.toString()})
+        expect(resp.raw.status).toBe(204)
+    })
+});
--- _playwright-tests/tests/auth.setup.ts
+++ _playwright-tests/tests/auth.setup.ts
@@ -2,12 +2,17 @@ import { expect, test as setup } from '@playwright/test';
 import { setAuthorizationHeader, throwIfMissingEnvVariables } from './helpers/loginHelpers';
 import { describe } from 'node:test';
 
-describe('Setup', async () => {
-  setup('Ensure needed ENV variables exist', async () => {
-    expect(() => throwIfMissingEnvVariables()).not.toThrow();
-  });
+const DefaultOrg = 99999
+const DefaultUser = "BananaMan"
 
-  setup('Authenticate', async () => {
-    await setAuthorizationHeader('BananaMan', 99999);
-  });
-});
+describe("Setup", async () => {
+    setup('Ensure needed ENV variables exist', async () => {
+        expect(() => throwIfMissingEnvVariables()).not.toThrow()
+    })
+
+    setup('Authenticate', async ({ page }) => {
+        if (!process.env["IDENTITY_HEADER"]) {
+            await setAuthorizationHeader(DefaultUser, DefaultOrg)
+        }
+    })
+})
