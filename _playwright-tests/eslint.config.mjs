30331f540f19074a20f9ce62a5a49e077d3e65c2
Dominik Vagner <dvagner@redhat.com>
2025-02-13 23:05:33 +0100

    Test: add format and lint checks to PW tests

---
+++ _playwright-tests/eslint.config.mjs
@@ -0,0 +1,68 @@
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import prettier from 'eslint-plugin-prettier';
+import unusedImports from 'eslint-plugin-unused-imports';
+import globals from 'globals';
+import tsParser from '@typescript-eslint/parser';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+import js from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
+  allConfig: js.configs.all,
+});
+
+export default [
+  {
+    ignores: ['node_modules/*', 'static/*', 'dist/*', 'tests/API/client/*'],
+  },
+  ...compat.extends(
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ),
+  {
+    plugins: {
+      '@typescript-eslint': typescriptEslint,
+      prettier,
+      'unused-imports': unusedImports,
+    },
+
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+
+      parser: tsParser,
+      ecmaVersion: 12,
+      sourceType: 'module',
+
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    settings: {
+      'import/resolver': {
+        typescript: {},
+      },
+    },
+
+    rules: {
+      'arrow-body-style': ['error', 'as-needed'],
+      camelcase: 'off',
+      'spaced-comment': 'error',
+      'prettier/prettier': ['warn', { singleQuote: true }],
+      'no-duplicate-imports': 'error',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': ['warn'],
+    },
+  },
+];
