a184f370dcf927d53419a8dd6f8dd9cf5ab3ff02
adewar <andrewgdewar@gmail.com>
2025-02-18 14:52:57 -0700

    Update introspection test

--- _playwright-tests/tests/API/Repositories.spec.ts
+++ _playwright-tests/tests/API/Repositories.spec.ts
@@ -1,16 +1,38 @@
 import { test } from './base_client';
-import { RepositoriesApi, GetRepositoryRequest, ApiRepositoryResponse } from './client';
+import {
+  RepositoriesApi,
+  GetRepositoryRequest,
+  ApiRepositoryResponse,
+  type ListRepositoriesRequest,
+} from './client';
 import { expect } from '@playwright/test';
 import { poll } from './apiHelpers';
 
 test('Content > Verify repository introspection', async ({ client }) => {
-  const repo = await new RepositoriesApi(client).createRepository({
-    apiRepositoryRequest: {
-      name: 'test-repository',
-      url: 'https://rverdile.fedorapeople.org/dummy-repos/modules/repo1/',
-    },
+  await test.step('delete existing repository if exists', async () => {
+    const existing = await new RepositoriesApi(client).listRepositories(<ListRepositoriesRequest>{
+      search: 'test-repository',
+    });
+
+    if (existing?.data?.length) {
+      const resp = await new RepositoriesApi(client).deleteRepositoryRaw(<GetRepositoryRequest>{
+        uuid: existing.data[0].uuid?.toString(),
+      });
+      expect(resp.raw.status).toBe(204);
+    }
   });
-  expect(repo.name).toBe('test-repository');
+
+  let repo: ApiRepositoryResponse;
+  await test.step('Create a repo with name test-repository', async () => {
+    repo = await new RepositoriesApi(client).createRepository({
+      apiRepositoryRequest: {
+        name: 'test-repository',
+        url: 'https://rverdile.fedorapeople.org/dummy-repos/modules/repo1/',
+      },
+    });
+
+    expect(repo.name).toBe('test-repository');
+  });
 
   await test.step('wait for introspection to be completed', async () => {
     const getRepository = () =>
