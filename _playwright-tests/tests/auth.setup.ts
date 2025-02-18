4117783b53446b4b94a142e3fbc23d26dc672ad2
adewar <andrewgdewar@gmail.com>
2025-02-18 10:52:47 -0700

    Rebase from master

--- _playwright-tests/tests/auth.setup.ts
+++ _playwright-tests/tests/auth.setup.ts
@@ -2,17 +2,17 @@ import { expect, test as setup } from '@playwright/test';
 import { setAuthorizationHeader, throwIfMissingEnvVariables } from './helpers/loginHelpers';
 import { describe } from 'node:test';
 
-const DefaultOrg = 99999
-const DefaultUser = "BananaMan"
+const DefaultOrg = 99999;
+const DefaultUser = 'BananaMan';
 
-describe("Setup", async () => {
-    setup('Ensure needed ENV variables exist', async () => {
-        expect(() => throwIfMissingEnvVariables()).not.toThrow()
-    })
+describe('Setup', async () => {
+  setup('Ensure needed ENV variables exist', async () => {
+    expect(() => throwIfMissingEnvVariables()).not.toThrow();
+  });
 
-    setup('Authenticate', async ({ page }) => {
-        if (!process.env["IDENTITY_HEADER"]) {
-            await setAuthorizationHeader(DefaultUser, DefaultOrg)
-        }
-    })
-})
+  setup('Authenticate', async () => {
+    if (!process.env['IDENTITY_HEADER']) {
+      await setAuthorizationHeader(DefaultUser, DefaultOrg);
+    }
+  });
+});
--- _playwright-tests/tests/helpers/apiHelpers.tsx
+++ _playwright-tests/tests/helpers/apiHelpers.tsx
@@ -1,13 +1,17 @@
 // while condition is true, calls fn, waits interval (ms) between calls.
 // condition's parameter should be the result of the function call.
-export const poll = async (fn: () => Promise<any>, condition: (result: any) => boolean, interval: number) => {
-    let result = await fn()
-    while (condition(result)) {
-        result = await fn()
-        await timer(interval)
-    }
-    return result
-}
+export const poll = async (
+  fn: <T>() => Promise<T>,
+  condition: <T>(result: T) => boolean,
+  interval: number,
+) => {
+  let result = await fn();
+  while (condition(result)) {
+    result = await fn();
+    await timer(interval);
+  }
+  return result;
+};
 
 // timer in ms, must await e.g. await timer(num)
-export const timer = (ms: number) => new Promise(res => setTimeout(res, ms))
+export const timer = (ms: number) => new Promise((res) => setTimeout(res, ms));
