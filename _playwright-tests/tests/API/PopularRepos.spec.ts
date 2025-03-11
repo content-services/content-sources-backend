41dec6c58da5c32403b93051f6d5315c021c4125
Justin Sherrill <jsherril@redhat.com>
2025-03-11 08:15:37 -0400

    test playwright test running

--- _playwright-tests/tests/API/PopularRepos.spec.ts
+++ _playwright-tests/tests/API/PopularRepos.spec.ts
@@ -7,7 +7,7 @@ test.describe('Populat repositories', () => {
     const resp = await new PopularRepositoriesApi(client).listPopularRepositories({
       search: 'EPEL 9',
     });
-    expect(resp.meta?.count).toBe(1);
+    expect(resp.meta?.count).toBe(0);
     expect(resp.data?.[0].suggestedName).toBe('EPEL 9 Everything x86_64');
   });
 });
