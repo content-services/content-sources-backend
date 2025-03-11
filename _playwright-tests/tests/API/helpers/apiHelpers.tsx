b396d674994aec3758e3dd5177683f9eaab52bd6
DewardianDev <andrewgdewar@gmail.com>
2025-03-11 12:11:33 -0600

    Chore: Update CI (#1017)
    * Chore: Update CI
    
    * Fix lint errors
    
    * Yarn install (dependency auto-update)

--- _playwright-tests/tests/API/helpers/apiHelpers.tsx
+++ _playwright-tests/tests/API/helpers/apiHelpers.tsx
@@ -1,13 +1,18 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // while condition is true, calls fn, waits interval (ms) between calls.
 // condition's parameter should be the result of the function call.
-export const poll = async (fn: () => Promise<any>, condition: (result: any) => boolean, interval: number) => {
-    let result = await fn()
-    while (condition(result)) {
-        result = await fn()
-        await timer(interval)
-    }
-    return result
-}
+export const poll = async (
+  fn: () => Promise<any>,
+  condition: (result: any) => boolean,
+  interval: number,
+) => {
+  let result = await fn();
+  while (condition(result)) {
+    result = await fn();
+    await timer(interval);
+  }
+  return result;
+};
 
 // timer in ms, must await e.g. await timer(num)
-export const timer = (ms: number) => new Promise(res => setTimeout(res, ms))
+export const timer = (ms: number) => new Promise((res) => setTimeout(res, ms));
