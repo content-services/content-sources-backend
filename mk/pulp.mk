1857101b893b6b66594a503afa3f3233d0f9a402
Justin Sherrill <jlsherrill@gmail.com>
2025-03-12 12:21:13 -0400

    Build: use full path for compose files (#973)

--- mk/pulp.mk
+++ mk/pulp.mk
@@ -1,5 +1,7 @@
-CS_COMPOSE_FILE ?= "deployments/docker-compose.yml"
-PULP_COMPOSE_FILES ?= "compose_files/pulp/docker-compose.yml"
+
+CS_COMPOSE_FILE ?= $(realpath deployments/docker-compose.yml)
+PULP_COMPOSE_FILES ?= $(realpath compose_files/pulp/docker-compose.yml)
+
 PULP_COMPOSE_OPTIONS=PULP_POSTGRES_PATH="pulp_db" PULP_STORAGE_PATH="pulp_storage"
 
 PULP_COMPOSE_COMMAND=$(PULP_COMPOSE_OPTIONS) $(DOCKER)-compose --project-name=$(COMPOSE_PROJECT_NAME)  -f $(PULP_COMPOSE_FILES) up --detach
