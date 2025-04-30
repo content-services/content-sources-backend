64644286eaa170a4eb75a04c56e620d78b0d02d0
swadeley <swadeley@redhat.com>
2025-04-30 09:42:16 +0100

    Test: Change to centirepos (#1069)
    Change to centirepos
    
    without number in URL

--- _playwright-tests/test-utils/src/helpers/repositories.ts
+++ _playwright-tests/test-utils/src/helpers/repositories.ts
@@ -1,13 +1,12 @@
 export const randomName = () => (Math.random() + 1).toString(36).substring(2, 6);
 
-export const rank = () => Math.floor(Math.random() * 10 + 1).toString();
 export const randomNum = () =>
-  Math.floor(Math.random() * 10 + 1)
+  Math.floor(Math.random() * 100 + 1)
     .toString()
     .padStart(2, '0');
 
 export const randomUrl = () =>
-  `https://stephenw.fedorapeople.org/multirepos/${rank()}/repo${randomNum()}/`;
+  `https://content-services.github.io/fixtures/yum/centirepos/repo${randomNum()}/`;
 
 export const SmallRedHatRepoURL =
   'https://cdn.redhat.com/content/dist/rhel9/9/aarch64/codeready-builder/os/';
