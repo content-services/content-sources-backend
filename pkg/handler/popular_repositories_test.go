06fd2bd69abf8e9e40f3967ca1c794848d1a8fbf
Bryttanie House <bhouse@redhat.com>
2025-05-13 12:47:04 -0400

    add rhel10 and epel10 repos

--- pkg/handler/popular_repositories_test.go
+++ pkg/handler/popular_repositories_test.go
@@ -82,6 +82,7 @@ func (s *PopularReposSuite) TestPopularRepos() {
 	paginationData := api.PaginationData{Limit: 1}
 	s.dao.RepositoryConfig.WithContextMock().On("List", test.MockCtx(), test_handler.MockOrgId, paginationData, api.FilterData{Search: "https://dl.fedoraproject.org/pub/epel/9/Everything/x86_64/"}).Return(collection, int64(0), nil)
 	s.dao.RepositoryConfig.On("List", test.MockCtx(), test_handler.MockOrgId, paginationData, api.FilterData{Search: "https://dl.fedoraproject.org/pub/epel/8/Everything/x86_64/"}).Return(collection, int64(0), nil)
+	s.dao.RepositoryConfig.On("List", test.MockCtx(), test_handler.MockOrgId, paginationData, api.FilterData{Search: "https://dl.fedoraproject.org/pub/epel/10/Everything/x86_64/"}).Return(collection, int64(0), nil)
 
 	path := fmt.Sprintf("%s/popular_repositories/?limit=%d", api.FullRootPath(), 10)
 	req := httptest.NewRequest(http.MethodGet, path, nil)
@@ -97,9 +98,9 @@ func (s *PopularReposSuite) TestPopularRepos() {
 	assert.Nil(s.T(), err)
 	assert.Equal(s.T(), http.StatusOK, code)
 	assert.Equal(s.T(), 0, response.Meta.Offset)
-	assert.Equal(s.T(), int64(2), response.Meta.Count)
+	assert.Equal(s.T(), int64(3), response.Meta.Count)
 	assert.Equal(s.T(), 10, response.Meta.Limit)
-	assert.Equal(s.T(), 2, len(response.Data))
+	assert.Equal(s.T(), 3, len(response.Data))
 	assert.Equal(s.T(), response.Data[0].ExistingName, "")
 }
 
