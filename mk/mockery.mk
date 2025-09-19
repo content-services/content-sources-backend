1dfd9aa4218f58a98e715b33bbed24a619586e09
Jakub Dugovič <jdugovic@redhat.com>
2025-09-19 11:38:31 +0200

    HMS-8889: update Mockery config to v3
    In v3, there are 2 main changes that affect us:
    - the mock files are generated per package (used to be per interface)
    - expecters are generated by default and cannot be disabled anymore

--- mk/mockery.mk
+++ mk/mockery.mk
@@ -1,6 +1,4 @@
-# MOCKERY_VERSION := $(shell curl -L https://api.github.com/repos/vektra/mockery/releases/latest | jq --raw-output .tag_name | sed 's/^v//')
-# TODO: When the mockery config is updated to be v3 compatible. Remove the line bellow which pins the version to "v2.*" and uncomment the one above.
-MOCKERY_VERSION := $(shell curl -L https://api.github.com/repos/vektra/mockery/tags\?per_page=1000 | grep "\"name\": \"v2." | head -n 1 | sed 's/.*"v\([^"]*\)".*/\1/')
+MOCKERY_VERSION := $(shell curl -L https://api.github.com/repos/vektra/mockery/releases/latest | jq --raw-output .tag_name | sed 's/^v//')
 
 export GO_OUTPUT
 
@@ -11,5 +9,5 @@ $(GO_OUTPUT)/mockery: ## Install mockery locally on your GO_OUTPUT (./release) d
 .PHONY: mock
 mock: $(GO_OUTPUT)/mockery ## Install mockery if it isn't already in ./release directory and regenerate mocks
 	./scripts/mockery_comment_fix.sh comment
-	$(GO_OUTPUT)/mockery
+	$(GO_OUTPUT)/mockery --config .mockery_v3.yml
 	./scripts/mockery_comment_fix.sh uncomment
