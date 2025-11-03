4247f8e9f936baf144843be1be1c33df5d56429d
mlahane <mlahane@redhat.com>
2025-11-03 17:30:38 -0500

    test: add test to verify that RHEL repos are always snapshotted.

--- _playwright-tests/tests/API/Snapshots.spec.ts
+++ _playwright-tests/tests/API/Snapshots.spec.ts
@@ -388,4 +388,25 @@ test.describe('Snapshots', () => {
       expect(repo.lastSnapshotUuid).toBeDefined();
     });
   });
+
+  test('Verify RHEL repos are snapshotted', async ({ client }) => {
+    const repositoriesApi = new RepositoriesApi(client);
+
+    await test.step('Get RHEL repositories', async () => {
+      const rhelRepos =
+        (
+          await repositoriesApi.listRepositories({
+            origin: 'red_hat',
+          })
+        ).data || [];
+      expect(rhelRepos.length).toBeGreaterThan(0);
+
+      for (const repo of rhelRepos) {
+        expect(
+          repo.lastSnapshotTask?.status === 'completed' ||
+            repo.lastSnapshotTask?.status === 'running',
+        ).toBe(true);
+      }
+    });
+  });
 });
