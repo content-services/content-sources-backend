4497f7d4557676fa95a10b68873a68f8e4cb30db
rverdile <ryanverdile@gmail.com>
2026-01-07 13:44:15 -0500

    remove pool limit for number of workers
    the workers no longer block 1 connection each, so the number of
    connections in the pool can be lower than the number of workers

--- pkg/tasks/queue/pgqueue.go
+++ pkg/tasks/queue/pgqueue.go
@@ -218,9 +218,6 @@ func NewPgxPool(ctx context.Context, url string) (*pgxpool.Pool, error) {
 	if poolLimit < math.MinInt32 || poolLimit > math.MaxInt32 {
 		return nil, errors.New("invalid pool limit size")
 	}
-	if poolLimit <= config.Get().Tasking.WorkerCount {
-		return nil, errors.New("pool size too small for the number of workers ")
-	}
 
 	pxConfig.MaxConns = int32(poolLimit)
 	if err != nil {
