59f91042d4784c9774e38f2978c3b6ba7205fe81
swadeley <swadeley@redhat.com>
2026-01-30 14:03:50 +0800

    Add coverage collection fixture to test-utils

--- _playwright-tests/test-utils/src/fixtures/index.ts
+++ _playwright-tests/test-utils/src/fixtures/index.ts
@@ -1,6 +1,7 @@
 export * from './blockAnalytics';
 export * from './cleanup';
 export * from './client';
+export * from './coverage';
 export * from './db';
 export * from './tokenRefresh';
 export * from './test';
