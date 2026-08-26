500fa1cf23eba31b2411c7db09ea201e8146d3da
etsien <etsien@users.noreply.github.com>
2026-08-26 12:46:05 -0400

    revert and delete duplicates sql files

--- db/migrations/20260818120000_add_lightwell_advisory_severity_order.down.sql
+++
@@ -1,7 +0,0 @@
-BEGIN;
-
-DROP INDEX IF EXISTS idx_lightwell_advisories_package_name;
-DROP INDEX IF EXISTS idx_lightwell_advisories_severity_order;
-ALTER TABLE lightwell_advisories DROP COLUMN IF EXISTS severity_order;
-
-COMMIT;
