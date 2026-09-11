cd78798c8f53e571a08db07b6728bd8771261bd2
Dominik Vagner <dvagner@redhat.com>
2026-09-11 10:20:58 +0200

    test: add is_internal arg to header gen script
    This adds a new argument to the `scripts/header.sh` script, which
    enables setting the `is_internal` flag for the user.

--- scripts/header.sh
+++ scripts/header.sh
@@ -3,19 +3,18 @@
 ORG_ID="$1"
 USER_NAME="$2"
 ACCOUNT_ID="$3"
-
+IS_INTERNAL="$4"
 
 function print_out_usage {
-	cat <<EOF
-Usage: ./scripts/header.sh <org_id> [user_name]
+  cat <<EOF
+Usage: ./scripts/header.sh <org_id> [user_name] [account_id] [is_internal]
 EOF
 }
 
 function error {
-	local err=$?
-	print_out_usage >&2
-	printf "error: %s\n" "$*" >&2
-	exit $err
+  print_out_usage >&2
+  printf "error: %s\n" "$*" >&2
+  exit 1
 }
 
 [ "${ORG_ID}" != "" ] || error "ORG_ID is required and cannot be empty"
@@ -28,14 +27,26 @@ if [ "${USER_NAME}" == "" ]; then
   USER_NAME="snapUser"
 fi
 
-case "$( uname -s )" in
-"Darwin" )
-  ENC="$(echo "{\"identity\":{\"org_id\":\"${ORG_ID}\", \"type\":\"User\",\"user\":{\"username\":\"${USER_NAME}\",\"user_id\":\"${USER_NAME}\"},\"account_number\":\"${ACCOUNT_ID}\",\"internal\":{\"org_id\":\"${ORG_ID}\"}}}" | base64 -b 0)"
-;;
+if [ "${IS_INTERNAL}" == "" ]; then
+  IS_INTERNAL=false
+fi
+
+case "${IS_INTERNAL}" in
+true | false)
+  ;;
+*)
+  error "IS_INTERNAL must be true or false"
+  ;;
+esac
+
+case "$(uname -s)" in
+"Darwin")
+  ENC="$(echo "{\"identity\":{\"org_id\":\"${ORG_ID}\", \"type\":\"User\",\"user\":{\"username\":\"${USER_NAME}\",\"user_id\":\"${USER_NAME}\",\"is_internal\":${IS_INTERNAL}},\"account_number\":\"${ACCOUNT_ID}\",\"internal\":{\"org_id\":\"${ORG_ID}\"}}}" | base64 -b 0)"
+  ;;
 
 "Linux" | *)
-  ENC="$(echo "{\"identity\":{\"org_id\":\"${ORG_ID}\", \"type\":\"User\",\"user\":{\"username\":\"${USER_NAME}\",\"user_id\":\"${USER_NAME}\"},\"account_number\":\"${ACCOUNT_ID}\",\"internal\":{\"org_id\":\"${ORG_ID}\"}}}" | base64 -w0)"
-;;
+  ENC="$(echo "{\"identity\":{\"org_id\":\"${ORG_ID}\", \"type\":\"User\",\"user\":{\"username\":\"${USER_NAME}\",\"user_id\":\"${USER_NAME}\",\"is_internal\":${IS_INTERNAL}},\"account_number\":\"${ACCOUNT_ID}\",\"internal\":{\"org_id\":\"${ORG_ID}\"}}}" | base64 -w0)"
+  ;;
 esac
 
 echo "x-rh-identity: $ENC"
