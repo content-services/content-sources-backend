f87f6022aa6b9e95394de03d7124c6689cd65ccd
swadeley <swadeley@redhat.com>
2026-09-14 10:54:52 +0100

    Set GOTOOLCHAIN=auto
    sqlc 1.31.1 needs Go 1.26 but repo is on Go 1.25.8

--- mk/sqlc.mk
+++ mk/sqlc.mk
@@ -2,8 +2,9 @@
 # sqlc code generation
 ##
 
+# sqlc 1.31.1 needs Go 1.26; GOTOOLCHAIN=auto lets `go run` fetch it when setup-go pins local.
 SQLC ?= go run github.com/sqlc-dev/sqlc/cmd/sqlc@v1.31.1
 
 .PHONY: sqlc-generate-lightwell
 sqlc-generate-lightwell: ## Generate sqlc store for lightwell vulnerabilities
-	cd "$(PROJECT_DIR)/pkg/lightwell/db" && $(SQLC) generate
+	cd "$(PROJECT_DIR)/pkg/lightwell/db" && GOTOOLCHAIN=auto $(SQLC) generate
